d79aef77bed20b26d9d9cc3f15d5fd45558d91ce
Araceli Cueva <aycuevam@gmail.com>
2018-02-14 00:01:25 -0500

    subiendo estructuta de carpetas

---
+++ README.md
@@ -0,0 +1,9 @@
+# Proyecto XHR-DEMO
+
+## Objetivos
+
+    Poner en practica el uso de AJAX, esencialmente haciendo uso de XMLHttpRequest.
+    
+## Resultados
+
+ ![Proyecto Nuestras Coders](assets/imgs/results/result.png "Nuestras Coders")
---
+++ public/js/app.js
@@ -0,0 +1,40 @@
+alert('hola');
+window.onload = () => {
+  const form = document.getElementById('search-form');
+  const searchField = document.getElementById('search-keyword');
+  const responseContainer = document.getElementById('response-container');
+  let searchedForText;
+  let cl = console.log();
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    responseContainer.innerHTML = '';
+    searchedForText = searchField.value;
+    getNews();
+  });
+
+  getNews = () => {
+    const articleRequest = new XMLHttpRequest();
+    articleRequest.open('GET', `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=65082f4668484e9484931f976feb3d4c`);
+    articleRequest.onload = addNews;
+    articleRequest.onerror = handleError;
+    articleRequest.send();
+  }
+
+  handleError = () => {
+    cl('se ha presentado un error');
+  }
+
+  addNews = () => {
+    const data = JSON.parse(this.responseText);
+    const response = data.response.docs;
+    response.forEach(function(element) {
+      cl(element);
+      const snippet = element.snippet;
+      let li = document.createElement('li');
+      li.className = 'articleClass';
+      li.innerText = snippet;
+      responseContainer.appendChild(li);
+    });
+  }
+};
